9c221419961610d1745e2668554a96cc64275433
Niloufar <mohammadiniloofar80@gmail.com>
2026-06-09 14:20:37 +0330

    creat slider and categories

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ hamber.addEventListener('click', () => {
   nav.classList.toggle('-translate-y-3');
  hamber.classList.toggle('icon-hamburger-menu-linear');
   hamber.classList.toggle('icon-close-linear');
+
+  const submenu = accor.nextElementSibling
+  submenu.style.maxHeight=submenu.scrollHeight+"px"
   
 });
  console.log(window.innerWidth)
@@ -31,3 +34,83 @@ accor.forEach(item=>{
         }
     })
 })
+
+/////////////////////////////////////
+
+const head = document.getElementById("head");
+
+window.addEventListener("scroll", () => {
+  head.classList.toggle("py-4", window.scrollY > 50);
+});
+
+
+//////////////slider/////////////////
+
+const swiper = new Swiper(".mySwipper", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  loop: true,
+  centeredSlides: false,
+
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  effect: "slide",
+
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 2,
+    slideShadows: false,
+  },
+
+ 
+  
+});
+
+//////////////////////////////////////
+
+const slider = document.querySelector(".category-slider");
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("cursor-grabbing");
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("cursor-grabbing");
+});
+
+slider.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("cursor-grabbing");
+});
+
+slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2; 
+    slider.scrollLeft = scrollLeft - walk;
+});
